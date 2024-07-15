7e4f174a727068655f310eccd7a5d8d37f25751a
Lennart Balkenhol <lennart@MacBook-Pro-136.local>
2024-07-15 15:50:09 +0200

    managed to evaluate a logl

--- likelihood/candl/candl_cosmosis_interface.py
+++ likelihood/candl/candl_cosmosis_interface.py
@@ -1,11 +1,12 @@
 try:
+    from candl.lib import *
     import candl
     import candl.data
 except ImportError:
     raise RuntimeError('Can not find candl. Try running: pip install candl-like')
 
-from cosmosis.datablock import option_section, names
-cosmo = names.cosmological_parameters
+from cosmosis.datablock import names, SectionOptions
+#cosmosis_names = names
 import numpy as np
 import os
 
@@ -20,11 +21,11 @@ class CandlCosmoSISLikelihood:
         """
 
         # Read requested data set from ini and grab a name under which the logl value will be recorded
-        data_set_str = options.get_string(option_section, "data_set", default="SPT3G_20192020_lensing_GMV")
+        data_set_str = options.get_string("data_set", default="SPT3G_20192020_lensing_GMV")
         if data_set_str[:11] == "candl.data.":
             # This looks like a short-cut to a pre-installed data set, e.g. "candl.data.SPT3G_2018_Lens"
             self.data_set_file = eval(data_set_str)
-            self.name = data_set_str
+            self.name = "candl." + data_set_str.split(".")[-1]
         else:
             # Assume this to be a path pointing directly to a data set .yaml file
             self.data_set_file = data_set_str
@@ -32,10 +33,15 @@ class CandlCosmoSISLikelihood:
 
         # Read in other options from ini
         # Further options for fancy data model selection to come...
-        self.lensing = options.get_bool(option_section, 'lensing', default=True)
-        self.clear_internal_priors = options.get_bool(option_section, 'clear_internal_priors', default=True)
-        self.feedback = options.get_bool(option_section, 'feedback', default=True)
-        self.data_selection = options.get_string(option_section, "data_selection", default=None)
+        self.lensing = options.get_bool('lensing', default=True)
+        self.clear_internal_priors = options.get_bool('clear_internal_priors', default=True)
+        self.feedback = options.get_bool('feedback', default=True)
+
+        # Optional data selection
+        if options.has_value("data_selection"):
+            self.data_selection = options.get_string("data_selection", default=None)
+        else:
+            self.data_selection = None
 
         # Initialise the likelihood
         try:
@@ -67,15 +73,21 @@ class CandlCosmoSISLikelihood:
         
         # Load all cosmological parameters and add extra params so that candl understands
         # These should not be needed by candl, but it doesn't hurt in case anyone builds a likelihood that directly depends on cosmological parameters
-        names      = [param_name for param_sec, param_name in block.keys() if param_sec == 'cosmological_parameters']
-        model_dict = {par: block[('cosmological_parameters',par)] for par in names}
+        cos_par_names      = [param_name for param_sec, param_name in block.keys() if param_sec == 'cosmological_parameters']
+        model_dict = {par: block[('cosmological_parameters',par)] for par in cos_par_names}
         model_dict['H0']   = model_dict['h0']*100
         model_dict['ns']   = model_dict['n_s']
         model_dict['logA'] = model_dict['log1e10as']
         
         # Load all nuisance parameters
-        names      = [param_name for param_sec, param_name in block.keys() if param_sec == 'nuisance_parameters']
-        model_dict = {par: block[('nuisance_parameters',par)] for par in names}
+        nuisance_par_names      = [param_name for param_sec, param_name in block.keys() if param_sec == 'nuisance_parameters']
+
+        # Match any nuisance parameters in candl and restore right cases
+        for i, par in enumerate([p.lower() for p in self.candl_like.required_nuisance_parameters]):
+            if par in nuisance_par_names:
+                nuisance_par_names[i] = self.candl_like.required_nuisance_parameters[i]
+        model_dict = {par: block[('nuisance_parameters',par)] for par in nuisance_par_names}# CosmoSIS doesn't care about cases, so putting them in is easy
+
         
         # Read in Cls from CosmoSIS and save them in dict.
         # CosmoSIS outputs CAMB Cls in unit of l(l+1)/(2pi) even for pp
@@ -114,7 +126,7 @@ class CandlCosmoSISLikelihood:
         # Slot supplied CMB spectra into an array of zeros of the correct length
         # candl will optionally import JAX which should ensure the two methods below run
         model_dict['Dl'] = {'ell': np.arange(self.candl_like.ell_min, self.candl_like.ell_max + 1)}
-        for spec_type, spec in zip(["pp", "kk", "TT", "EE", "BB", "TE"] [cl_pp, cl_kk, cl_tt, cl_ee, cl_bb, cl_te]):
+        for spec_type, spec in zip(["pp", "kk", "TT", "EE", "BB", "TE"], [cl_pp, cl_kk, cl_tt, cl_ee, cl_bb, cl_te]):
             model_dict['Dl'][spec_type] = jnp.zeros(N_ell)
             model_dict['Dl'][spec_type] = jax_optional_set_element(
                 model_dict['Dl'][spec_type],
@@ -137,6 +149,5 @@ def setup(options):
 
 def execute(block, config):
     like    = config.likelihood(block)
-    dataset = config.dataset
-    block[names.likelihoods, "%s_like"%dataset] = like
+    block[names.likelihoods, "%s_like"%config.name] = like
     return 0
